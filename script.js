285bfbbf2d5cabc28b40a118c6c2fb4a05c4b122
Dudz <142178060+Odudz@users.noreply.github.com>
2025-08-19 09:39:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const pageInput = document.getElementById("pageInput")
+const searchBtn = document.getElementById("searchBtn")
+const resultsdiv = document.getElementById("results")
+
+async function fetchCharacters(page){
+    resultsdiv.innerHTML = "<p>Carregando....<p>"
+
+    try{
+
+    } catch(error){
+
+    }
+}
