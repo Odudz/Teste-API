d04017461481cb8cbc0b927c224557aa4058bc1b
Dudz <142178060+Odudz@users.noreply.github.com>
2025-08-19 09:43:50 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,7 +6,9 @@ async function fetchCharacters(page){
     resultsdiv.innerHTML = "<p>Carregando....<p>"
 
     try{
-
+        const response = await fetch("https://gravity-falls-api.vercel.app/api/characters")
+        const data = await response.json()
+        console.log(data)
     } catch(error){
 
     }
