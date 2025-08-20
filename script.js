1ed0244ea97fff8e4e4dc27b256db5630a306729
Dudz <142178060+Odudz@users.noreply.github.com>
2025-08-20 08:42:01 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,10 +6,34 @@ async function fetchCharacters(page){
     resultsdiv.innerHTML = "<p>Carregando....<p>"
 
     try{
-        const response = await fetch("https://gravity-falls-api.vercel.app/api/characters")
+    
+        const response = await fetch(`https://rickandmortyapi.com/api/character`)
         const data = await response.json()
         console.log(data)
+
+        if(data.error){
+            resultsdiv.innerHTML = "<p>Página inválida!Tente novamente.</p>"
+            return
+        }
+
+        resultsdiv.innerHTML = ""
+        data.results.forEach(character => {
+            const card = document.createElement("div")
+            card.className = "card"
+            card.innerHTML = `
+                <img src="${character.image}" alt="${character.name}">
+                <h3>${character.name}</h3>
+                <p><strong>Status:</strong>${character.status}</p>
+                <p><strong>Espécie:</strong>${character.species}</p>
+            `
+            resultsdiv.appendChild(card)
+        })
+    
     } catch(error){
 
+        console.log(error)
+    
     }
 }
+
+fetchCharacters(1)
