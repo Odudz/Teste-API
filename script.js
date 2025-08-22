60c1bc50dbd20d802d80afb9e89956648544c2c6
Dudz <142178060+Odudz@users.noreply.github.com>
2025-08-22 12:19:07 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,39 +1,36 @@
-const pageInput = document.getElementById("pageInput")
-const searchBtn = document.getElementById("searchBtn")
-const resultsdiv = document.getElementById("results")
+const resultsdiv = document.getElementById("results");
 
-async function fetchCharacters(page){
-    resultsdiv.innerHTML = "<p>Carregando....<p>"
+async function fetchCharacters() {
+    resultsdiv.innerHTML = "<p>Carregando....</p>";
 
-    try{
-    
-        const response = await fetch(`https://valorant-api.com/v1/agents`)
-        const data = await response.json()
-        console.log(data)
+    try {
+        const response = await fetch("https://valorant-api.com/v1/agents");
+        const data = await response.json();
 
-        if(data.error){
-            resultsdiv.innerHTML = "<p>Página inválida!Tente novamente.</p>"
-            return
-        }
+        resultsdiv.innerHTML = "";
 
-        resultsdiv.innerHTML = ""
-        data.results.forEach(agents => {
-            const card = document.createElement("div")
-            card.className = "card"
-            card.innerHTML = `
-                <img src="${agents.image}" alt="${agents.name}">
-                <h3>${agents.name}</h3>
-                <p><strong>Status:</strong>${agents.status}</p>
-                <p><strong>Espécie:</strong>${agents.species}</p>
-            `
-            resultsdiv.appendChild(card)
-        })
-    
-    } catch(error){
-        
-        //console.log(error)
-    
+        data.data
+    .filter(agent => agent.isPlayableCharacter)
+    .forEach(agent => {
+        const role = agent.role ? agent.role.displayName.toLowerCase() : "sem-role";
+        const roleClass = role.replace("í", "i").replace("é", "e").replace(" ", "-");
+
+        const card = document.createElement("div");
+        card.className = "card";
+        card.innerHTML = `
+            <img src="${agent.displayIcon}" alt="${agent.displayName}" class="${roleClass}">
+            <h3>${agent.displayName}</h3>
+            <p><strong>Função:</strong> ${agent.role ? agent.role.displayName : 'N/A'}</p>
+        `;
+
+        resultsdiv.appendChild(card);
+    });
+
+
+    } catch (error) {
+        console.error(error);
+        resultsdiv.innerHTML = "<p>Erro ao carregar dados da API.</p>";
     }
 }
 
-fetchCharacters(1)
+fetchCharacters();
